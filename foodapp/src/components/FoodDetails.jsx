38d3a5880ca834c25055da25e73126b2958b560e
QastTheLast <krapper9@gmail.com>
2024-02-02 11:43:13 +0400

    feat: add card desc fetch and style

---
+++ foodapp/src/components/FoodDetails.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import styles from "./fooddetails.module.css";
+
+export default function FoodDetails({ foodId }) {
+  const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "57f2fac41df74d33a76e719f0372607c";
+
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+      setIsLoading(false);
+    }
+    fetchFood();
+  }, [foodId]);
+
+  return (
+    <div>
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeName}>{food.title}</h1>
+        <img className={styles.recipeImage} src={food.image} alt="" />
+        <div className={styles.recipeDetails}>
+          <span>
+            <strong>🕐{food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            <strong>👪Serves {food.servings}</strong>
+          </span>
+          <span>
+            {" "}
+            <strong>
+              {food.vegetarian ? "🥕Vegetarian" : "🍖Non-vegetarian"}
+            </strong>
+          </span>
+          <span>
+            <strong>{food.vegan ? "🐮Vegan" : ""}</strong>
+          </span>
+        </div>
+        <div>
+          <span>
+            <strong>
+              {Math.round(food.pricePerServing / 100)}💲Per Serving
+            </strong>
+          </span>
+        </div>
+        <h2>Instructions</h2>
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              food.analyzedInstructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+}
